2cc23081b5897d74cb0e5642a351123972404a1a
ACH <albandecrevoisier@gmail.com>
2016-11-14 23:55:08 +0100

    Add first-params.c

---
+++ first_params.c
@@ -0,0 +1,27 @@
+/* first_params.c */
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static char *message = "Hello world!";
+module_param(message, charp, S_IRUGO);
+MODULE_PARM_DESC(message, "The message to print");
+
+static int __init first_init(void)
+{
+	pr_info("%s\n", message);
+	return 0;
+}
+
+static void __exit first_exit(void)
+{
+	pr_info("Goodbye, cruel world.\n");
+}
+
+module_init(first_init);
+module_exit(first_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("My first module with parameters");
+MODULE_AUTHOR("ach");
+
